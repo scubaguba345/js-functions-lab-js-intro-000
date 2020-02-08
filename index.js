5871b14e86aead065479536ac3ca4a2e246becea
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-02-08 01:03:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// write your code below!
+function happyHolidays() {
+  return "happyHolidays!"
+  happyHolidays()
+}// write your code below!
+function happyHolidaysTo(){
+  return (`Happy Holidays, ${name})!`
+  happyHolidaysTo()
+}
