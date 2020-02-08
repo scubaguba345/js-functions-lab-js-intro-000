19676913033f9c17fc0ed12c203ee89a06f81eff
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-02-08 01:15:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function happyHolidaysTo (name) {
     return `Happy holidays, ${name}!`;
   }
 
-  function happyHolidayTo (holiday, name) {
+  function happyCustomHolidayTo (holiday, name) {
     return "Happy " + `${holiday}` + ", " + `${name}` + "!";
     return `Happy ${holiday}, ${name}!`;
   }
-
+  
   function holidayCountdown (holiday, days) {
     return "It's " + `${days}` + " days until " + `${holiday}` + "!";
     return `It's ${days} days until ${holiday}!`;
